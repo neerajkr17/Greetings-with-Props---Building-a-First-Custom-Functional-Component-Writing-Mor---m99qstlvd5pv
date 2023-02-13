b65dfe535afb8ea5027a26fb3d32cf5f38a8e000
Neeraj Kumar <121371264+neerajkr17@users.noreply.github.com>
2023-02-13 22:07:16 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,15 @@
 // write code for Welcome component here
+import React from 'react';
+
+const Welcome = ({ name }) => {
+  return (
+    <div>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  );
+};
+
+export default Welcome;
+Footer
+© 2023 GitHub, Inc.
